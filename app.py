644f5fb6983579f60198745e1389854cb0d62ede
Matteo Santamaria <matteosantama@gmail.com>
2020-03-24 13:11:35 -0500

    start using argparser

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify, Response
 from slack import WebClient
 import json
 import os
+import argparse
 
 
-# configure slack client for handling requests
+# configure slack client by loading token from environment
 SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 slack_client = WebClient(token=SLACK_BOT_TOKEN)
 
@@ -13,23 +14,17 @@ app = Flask(__name__)
 
 
 @app.route('/slack/calculate', methods=['POST'])
-def calculate():
-    print('here we are')
+def receive_request():
+    parser = argparse.ArgumentParser(description='Short-term lending calculator', prog='rcalc')
+
+    parser.add_argument('rate', choices=['zq', 'sr1'], required=True)
+
+    args = parser.parse_args(request.form['text'])
+
 
     print(request.form)
 
     return 'no errors??'
-
-    # parse the request payload
-    # form_json = json.loads(request.form["payload"])
-
-    # print('here!')
-    # return Response(json.dumps(form_json), mimetype='application/json')
-    
-    # return jsonify(
-    #     response_type='in_channel',
-    #     text='<https://youtu.be/frszEJb0aOo|General Kenobi!>',
-    # )
 
 
 # start the Flask server
