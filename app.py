8af1b5af2022b8037fda98cbbb025d88888fe26e
Matteo Santamaria <matteosantama@gmail.com>
2020-03-24 13:46:35 -0500

    move custom argparser

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, request, jsonify, Response
 from slack import WebClient
 import json
 import os
-import argparser
+import sys
 import argparse
+
 
 
 # configure slack client by loading token from environment
@@ -14,15 +15,31 @@ slack_client = WebClient(token=SLACK_BOT_TOKEN)
 app = Flask(__name__)
 
 
+
+class ArgumentParser(argparse.ArgumentParser):
+    """
+    Custom implementation of argparse module that supports raising exceptions
+    on errors instead of just silently exiting
+    """
+
+    def error(self, message):
+        exc = sys.exc_info()[1]
+        if exc:
+            exc.argument = self._get_action_from_name(exc.argument_name)
+            raise exc
+        super(ArgumentParser, self).error(message)
+
+
+
 @app.route('/slack/calculate', methods=['POST'])
 def receive_request():
-    parser = argparser.ArgumentParser(description='Short-term lending calculator', prog='rcalc')
+    parser = argparse.ArgumentParser(description='Short-term lending calculator', prog='rcalc')
 
     parser.add_argument('rate', choices=['zq', 'sr1'], help='Specify whether to use zq or sr1')
 
     try:
         args = parser.parse_args(request.form['text'].split(' '))
-    except argparser.ArgumentError as err:
+    except argparse.ArgumentError as err:
         print(err)
         print('here')
         return 'errored'
